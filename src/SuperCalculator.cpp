11897fdbdc691cfaf8f011f48b2ce3098b3e89a6
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2016-03-02 21:18:59 -0500

    initial commit

---
+++ src/SuperCalculator.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : SuperCalculator.cpp
+ Author      : Marc-Olivier Arsenault & Eric Tremblay
+ Version     :
+ Copyright   : 
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
